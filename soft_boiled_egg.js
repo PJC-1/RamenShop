530c4aa932cb7cc83f330e3f69a37733b10b49d3
PJC-1 <jumpzine@gmail.com>
2018-11-03 15:09:10 -0700

    added egg script

---
+++ soft_boiled_egg.js
@@ -0,0 +1,32 @@
+/////////////////////
+// SOFT-BOILED EGG //
+/////////////////////
+
+var softBoiledEgg = {
+	description        : "Soft-boiled egg (ajitsuke tamago)",
+	cookingTime        : "~6.5 minutes",
+	notes              : {
+		1 : "Let eggs be room-temp before boiling",
+		2 : "Gentily crack the egg all over and then place back in water for ~1 hour, "
+        + "this will help with peeling the shell",
+		3 : "Generally takes about 12 minutes to fully hard boil an egg, so the "
+        + "soft-boiled egg is ~6-6.5 minutes",
+		4 : "You can use chashu marinade",
+		5 :  "Move eggs around for the first minute"
+	},
+	prepareAheadOfTime : true
+};
+
+var softBoiledEggIngredients = {
+	eggs  : "n",
+	soy   : "2 parts",
+	mirin : "1 part",
+	sake  : "1 part"
+};
+
+var softBoiledEggInstructions = {
+	1 : "Boil for 6.5 minutes",
+	2 : "Shock in ice water",
+	3 : "Peel eggs in water",
+	4 : "Soak in marinade for ~3-5 hours"
+};
