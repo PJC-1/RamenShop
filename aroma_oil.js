b7a38c8ddfd63855b19276626bae77e6488236f2
PJC-1 <jumpzine@gmail.com>
2018-11-03 15:08:29 -0700

    added recipe for mayu

---
+++ aroma_oil.js
@@ -0,0 +1,32 @@
+////////////////
+// AROMA-OIL  //
+////////////////
+
+var mayu = {
+	description        : "black garlic oil",
+	prepareAheadOfTime : true
+
+};
+
+var mayuIngredients = {
+	garlic       : "10 cloves peeled and minced (makes ~ 3.5 tbs)",
+	vegitableOil : "1/4 cup",
+	sesameOil    : "1/4 cup"
+};
+
+var mayuInstructions = {
+	1  : "Peel and mince the garlic",
+	2  : "Add oil to a frying pan",
+	3  : "Add garlic and cook at medium heat",
+	4  : "After they are slightly cooked lower the heat to low",
+	5  : "Cook for about 20 mins stirring frequently",
+	6  : "Garlic will slowly go from brown to black...do not over cook it will "
+       + "become bitter",
+	7  : "When most of the garlic is turning black it will start to get sticky",
+	8  : "Transfer to a heatproof dish",
+	9  : "Add sesame oil",
+	10 : "OPTIONAL: add chili oil",
+	11 : "Blend on high speed until pulverized",
+	12 : "Transfer to a resealable container",
+	13 : "Can store in the refrigerator for up to 2 months"
+};
