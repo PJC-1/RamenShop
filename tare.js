0337cfc07f38a77905b925ffc200d3b0fd6773ed
PJC-1 <jumpzine@gmail.com>
2018-11-02 22:58:43 -0700

    added script with tare recipe

---
+++ tare.js
@@ -0,0 +1,37 @@
+//////////
+// TARE //
+//////////
+
+var tareIngredients = {
+	kombu        : "5-7 3x3 inch squares",
+	waterToCover : "~1-2 cups",
+	niboshi      : "3/4 cup",
+	sesameOil    : "1 tbs",
+	bonitoFlakes : "1 cup loosely packed",
+	sake         : "1/4 cup",
+	mirin        : "1/4 cup",
+	soySauce     : "~1-1.5 cup",
+	salt         : "to taste"
+};
+
+var tareInstructions = {
+	1   : "Cover the kombu with water enough to cover. Let sit overnight, or at "
+        + "least 3 hours, in the fridge.",
+	2   : "When ready, add sesame oil to a medium sauce pan over medium heat.",
+	3   : "When oil is shimmering, toss in the niboshi, sauteeing in the oil for "
+        + "40 seconds to a minute, or until brown and fragrant. Try not to get "
+        + "this too hot, as bitter flavors might develop if the fish is "
+        + "overheated.",
+	4   : "Add the kombu and kombu soaking liquid. Bring to 176 degrees.",
+	5   : "Remove the kombu, add the bonito flakes, steep the niboshi and bonito in "
+        + "this liquid at 176 for 10 minutes.",
+	6   : "Strain broth, and reserve while making soy base.",
+	7   : "In the pot, add in the mirin and sake, and cook at a boil to remove "
+        + "the alcohol, around 5 minutes.",
+	8   : "Add in the soy sauce, and bring to 176 again. Let sit at this temp for a "
+        + "few minutes to concentrate.",
+	9   : "Add in an equal part of the fish broth made earlier. Season with salt "
+        + "until quite salty (like, to the point that youd go WOW this is almost "
+        + "too salty for me to handle, but damn its nice).",
+	10  : "Add 2-3 tbsp per bowl (according to taste or bowl size)."
+};
