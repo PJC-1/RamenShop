ea4761b4486877878775d59e96c2f59245f92082
PJC-1 <jumpzine@gmail.com>
2018-11-02 22:56:04 -0700

    added script for braised pork belly

---
+++ chashu.js
@@ -0,0 +1,54 @@
+////////////
+// CHASHU //
+////////////
+
+var chashu = {
+	type               : "pork belly",
+	price              : "$6.99 /Lb",
+	prepareAheadOfTime : true,
+	notes              : {
+		1 : "You can reheat the slices of pork belly in some ramen broth"
+	}
+};
+
+var chashuIngredients = {
+	porkBelly    : "4 lbs",
+	soy          : "1 cup(2 parts)",
+	mirin        : "1/2 cup(1 part)",
+	sake         : "1/2 cup (1 part)",
+	water        : "1 cup (2 part)",
+	greenOnion   : "1 bunch",
+	ginger       : "1 thumb-size piece",
+	garlic       : "~3-5 cloves",
+	leek         : 1,
+	sugar        : "pinch",
+	shaoxingWine : "1/4 cup",
+  cookingTwine : "n"
+};
+
+var chashuInstructionsPressureCooker = {
+	1 : "Roll up porkbelly",
+	2 : "Use twine to tightly tie porkbelly",
+	3 : "Blanch porkbelly ~10 minutes",
+	4 : "Add porkbelly, soy, mirin, sake, water, greenOnion, ginger, leek, "
+      + "shaoxing wine, and surgar into a pot",
+	5 : "Prep the pressure cooker, then cook on high pressure for ~60-90 minutes",
+	6 : "Heat a sauce pan over medium-high heat, when the pan is hot brown the "
+	    + "pork on all side",
+	7 : "Move contents to a container and place in the ridge too cool",
+	8 : "Remove any solid fatty oil at the top"
+};
+
+
+var chashuInstructions = {
+	1 : "Salt porkbelly",
+	2 : "Roll up porkbelly",
+	3 : "Sear on all sides",
+	4 : "Add porkbelly, soy, mirin, sake, water, greenOnion, ginger, leek, "
+      + "shaoxing wine, and surgar into a pot",
+	5 : "Bring to a boil, cover with foil (or Otoshi Buta,) cover with a drop lid",
+	6 : "Braise at 225 degrees (medium-low) in marinade for ~2-3 hours or until tender",
+	7 : "Occasionally turn, until there is 1/4 inch liquid left in the pot",
+	8 : "Use another pan to brown the porkbelly throughout",
+	9 : "Move contents to a container and place in the ridge to cool"
+};
