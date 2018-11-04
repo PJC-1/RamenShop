fdd5363ef33775d64bc35cfcbba48218aa1af120
PJC-1 <jumpzine@gmail.com>
2018-11-04 08:05:29 -0800

    added some miscellaneous notes

---
+++ misc.js
@@ -0,0 +1,71 @@
+/////////////
+// NOODLES //
+/////////////
+
+var noodles = {
+	name  : "Sun Ramen Noodles",
+	price : "$3.00 each"
+};
+
+////////////
+// DAY 01 //
+////////////
+
+// 1. Place Pork-Bones in a big bowl filled with water in the fridge over-night
+// 2. Cook Chashu and then place in fridge over-night
+// 3. Soak Kombu over night in the fridge
+// 4. Prepare mayu
+
+
+////////////
+// DAY 02 //
+////////////
+
+// 1. Cook eggs and marinate
+// 2. Rehidrate the shitake mushrooms
+// 3. Start the broth
+// 4. Prepare all toppings
+
+
+////////////////
+// VOCABULARY //
+////////////////
+
+var vocabulary = {
+	mayu        : "Black garlic oil",
+	niboshi     : "Dried baby sardines",
+	kombu       : "Dried kelp",
+	katsuobushi : "Dried fermented and smoked skipjack tuna (aka bonito flakes)"
+};
+
+///////////
+// NOTES //
+///////////
+
+var notes = [
+	{
+		q : "How many servings did the recipe yeild?",
+		a : "10 - 14 servings (depending on the size of the bowl)"
+	},
+	{
+		q : "What size pot if you use to cook the broth?",
+		a : "16 quart stockpot"
+	},
+	{
+		q : "Hesitant to add water during cooking?",
+		a : "Absolutely add more water during the actual cooking process. You need "
+        + "bones to be able to move around and be totally submerged"
+	},
+	{
+		q : "How much tare do you usually put in?",
+		a : "~ 2-3 tbs of tare per bowl. You can also add salt, find a balance."
+	},
+	{
+		q : "Weight for tare ingredients?",
+		a : {
+			niboshi     : "30-40 grams",
+			kombu       : "20-30 grams",
+			katsuoFlake : "25 grams"
+		}
+	}
+];
